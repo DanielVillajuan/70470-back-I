469625c6bbafa74fef16e60fd2dd18d3234aa552
Daniel Villajuan <danielvillajuan@hotmail.com>
2025-02-13 20:57:16 -0300

    fin de clase 14

---
+++ Clase-14/src/models/products.model.js
@@ -0,0 +1,14 @@
+import { model, Schema } from "mongoose";
+
+const productCollection = 'products'
+const productSchema = new Schema({
+    nombre: String,
+    codigo: {
+        type: String,
+        unique: true
+    },
+    stock: { type: Number, require: true },
+    tieneDescuento: Boolean,
+})
+
+export const ProductModel = model(productCollection, productSchema)
--- Clase-14/src/server.js
+++ Clase-14/src/server.js
@@ -1,9 +1,17 @@
 import express from "express";
 import ProductRouter from './routes/products.router.js'
+import mongoose from "mongoose";
+import dotenv from 'dotenv'
+dotenv.config()
 
+const stringConection = process.env.MONGO_KEY // lo reconoce gracias a dotenv
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/product', ProductRouter)
 
+mongoose
+    .connect(stringConection, { dbName: 'Almacen' })
+    .then(() => console.log('Conectado a la base de datos!'))
 app.listen(8080, () => console.log("server ok puerto 8080"));
